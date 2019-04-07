2bab58758447b1fd9b0469505276da0b5ab2068e
ejolly <eshin.jolly@gmail.com>
2019-04-06 23:30:34 -0400

    Working component rendering by location.

--- screens/WhereAreYou.js
+++ screens/WhereAreYou.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Platform, Text, View, StyleSheet } from 'react-native';
+import { Platform, Text, View, StyleSheet, ActivityIndicator } from 'react-native';
 import { Constants, Location, MapView, Permissions } from 'expo';
 import { Button, ThemeProvider } from 'react-native-elements'
 import db from '../components/Firebase'
@@ -26,12 +26,19 @@ export default class App extends Component {
       this.setState({
         errorMessage: 'Oops, this will not work on Sketch in an Android emulator. Try it on your device!',
       });
-    } else {
-      await this._getLocationAsync();
-      await this._getRestaurantsFromDB();
-      await this._getGoogleList();
-      await this._filterRestaurants();
-    }
+    } 
+      // await this._getLocationAsync();
+      // await this._getRestaurantsFromDB();
+      // await this._getGoogleList();
+      // await this._filterRestaurants();
+    
+  }
+
+  async componentDidMount() {
+    await this._getLocationAsync();
+    await this._getRestaurantsFromDB();
+    await this._getGoogleList();
+    await this._filterRestaurants();
   }
 
   _getLocationAsync = async () => {
@@ -44,19 +51,16 @@ export default class App extends Component {
 
     let location = await Location.getCurrentPositionAsync({});
     this.setState({ location: location });
+    this.state.location ? console.log("LOCATION SAVED") : console.log("NO LOCATION")
   };
 
-  _getRestaurantsFromDB = () => {
-    db.ref('Restaurants').once('value')
-    .then((snapshot) => {
-      let restaurants = Object.keys(snapshot.val());
-      this.setState({
-        restaurants: restaurants
-      })
+  _getRestaurantsFromDB = async () => {
+    const snapshot = await db.ref('Restaurants').once('value')
+    let restaurants = await Object.keys(snapshot.val());
+    this.setState({
+      restaurants: restaurants
     })
-    .catch((err) => {
-      console.log(err)
-    })
+    this.state.restaurants ? console.log("RESTAURANTS SAVED") : console.log("NO RESTAURANTS")
   }
 
   _getGoogleList = async () => {
@@ -67,44 +71,49 @@ export default class App extends Component {
       longitude = "-72.29490534567546"
       let results = await fetch("https://maps.googleapis.com/maps/api/place/nearbysearch/json?location="+latitude+","+longitude+"&radius=1500&type=restaurant&key=AIzaSyD1qY1K3HccJkYAcOULn9NDMPts2plwlA0")
       let resultsJSON = await results.json()
-      resultsJSON = await resultsJSON.results
+      resultsJSON = resultsJSON.results
       let googleResults = []
       resultsJSON.forEach( (res) => {
         googleResults.push({ 'id': res.id, 'name': res.name })
       })
+      // googleResults ? console.log(googleResults): console.log("NO FETCH")
       this.setState({
         googleResults: googleResults
       })
+      this.state.googleResults ? console.log("GOOGLE SAVED") : console.log("NO GOOGLE")
     } catch(err) {
       console.log(err)
     }
   }
 
   _filterRestaurants = async () => {
-    try {
+
       // filter by db restaurants
+      let googleResults = this.state.googleResults
+      let restaurants = this.state.restaurants
       let nearestRestaurants = [];
-      this.state.googleResults.forEach( (res) => {
-        if (this.state.restaurants.indexOf(res.name) !== -1){
+      googleResults.forEach( (res) => {
+        if (restaurants.indexOf(res.name) !== -1){
+          // console.log("ADDING: " + res.name)
           nearestRestaurants.push(res);
+        } else {
+          // console.log("SKIPPING: " + res.name)
         }
       }) 
       this.setState({
           nearestRestaurants: nearestRestaurants
         })
-    } catch(err) {
-      console.log(err)
-    }
+      this.state.nearestRestaurants ? console.log("NEAREST FILTERED") : console.log("NO FILTER")
+ 
   }
 
   render() {
     if (this.state.nearestRestaurants) { 
-      console.log(this.state.nearestRestaurants)
       return (
         <View style={styles.container}>
           {this.state.nearestRestaurants.map((res) => {
             return (
-              <ThemeProvider theme={theme}>
+              <ThemeProvider theme={theme} key={res.id}>
                 <Button
                   onPress = {this._onPressButton}
                   title={res.name}
@@ -118,8 +127,12 @@ export default class App extends Component {
           </View>
         )
     } else {
-      console.log(this.state)
-      return null
+        return (
+          <View style={[styles.container, styles.horizontal]}>
+            <ActivityIndicator size="large" color="#0000ff" />
+          </View>
+        )
+      
     }
   }
 }
@@ -140,6 +153,18 @@ const styles = StyleSheet.create({
     textAlign: 'center',
   },
 });
+
+const loadingstyles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center'
+  },
+  horizontal: {
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    padding: 10
+  }
+})
 
 // Styling for specific react-native-elements components
 const theme = {
